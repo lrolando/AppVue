200603b761588ad55b4228b2f11281030a688294
luciano Rolando <lucianorolando279@gmail.com>
2022-04-21 06:28:13 -0300

    adding Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+import RadioButtons from './components/RadioButtons.vue'
 </script>
 
 <template>
@@ -13,7 +13,7 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
-    <TheWelcome />
+    <RadioButtons/>
   </main>
 </template>
 
